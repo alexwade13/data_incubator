80e4167df649183a62d2e0cd02e60297961d6ff3
Alex Wade <alex.wade13@gmail.com>
2017-11-30 16:08:47 -0500

    heroku push?

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from datetime import datetime
 app = Flask(__name__)
+
+@app.route('/img/', methods=['POST','GET'])
+
+def imgDisplay():
+    print(request.files['picture'],"TESTSTS")
+    image = request.files['picture'].read()
+    
+    return """
+    <h1>Hello heroku</h1>
+    <body cz-shortcut-listen="true">
+        <h1> Automatically deployed! </h1>
+        test
+        <img src="{image}"/>
+    </body>
+    
+
+    """
+
+
+@app.route('/hello')
+def hellodd():
+    return 'Hello, World'
 
 @app.route('/')
 def homepage():
@@ -8,6 +30,14 @@ def homepage():
 
     return """
     <h1>Hello heroku</h1>
+    <body cz-shortcut-listen="true">
+        <h1> Automatically deployed! </h1>
+        <form action="/img/" method="post" enctype="multipart/form-data">
+            <input type="file" name="picture" accept="image/*">
+            <input type="submit">
+        </form>
+    
+    </body>
     <p>It is currently {time}.</p>
 
     """.format(time=the_time)
